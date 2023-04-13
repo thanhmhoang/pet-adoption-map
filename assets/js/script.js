7229958d170132b0064d576b65de6e67113859fa
ChickenChamp71 <chickensandsharks@gmail.com>
2023-04-13 13:50:37 -0700

    added geocoder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,25 @@
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2hpY2tlbmNoYW1wNzEiLCJhIjoiY2xnNnlzeXRsMDh2OTNlcGEwNTIxemJlYiJ9.uV30dsqz49aMZVVR3s6mrg';
-    const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
+// mapboxgl.accessToken = 'pk.eyJ1IjoiY2hpY2tlbmNoYW1wNzEiLCJhIjoiY2xnNnlzeXRsMDh2OTNlcGEwNTIxemJlYiJ9.uV30dsqz49aMZVVR3s6mrg';
+//     const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//     center: [-74.5, 40], // starting position [lng, lat]
+//     zoom: 9 // starting zoom
+// });
 
-map.on('load', () => {
-  // Add the vector tileset as a source.
-  map.addSource('ethnicity', {
-    type: 'vector',
-    url: 'mapbox://examples.8fgz4egr'
+
+
+var pf = new petfinder.Client({apiKey: "H1zZ78gYCiKq46kUPJnIyVFzyMzZUUZsjPEXGgl1GZNVoync7C", secret: "TdqKye1nvcjk7DuqueR6Av1KteGdSCChuqnZe68t"});
+
+pf.organization.search({location: "Seattle, WA"})
+  .then(function (response) {
+    for (let i = 0; i < response.data.organizations.length; i++) {
+    console.log(response.data.organizations[i]);
+    }
   });
-  map.addLayer( {
-    'id': 'population',
-    'type': 'circle',
-    'source': 'ethnicity',
-    'source-layer': 'sf2010',
-    'paint': {
-  // Make circles larger as the user zooms from z12 to z22.
-    'circle-radius': {
-    'base': 1.75,
-    'stops': [
-      [12, 2],
-      [22, 180]
-    ]
-  },
-  // Color circles by ethnicity, using a `match` expression.
-  'circle-color': [
-  'match',
-  ['get', 'ethnicity'],
-  'White',
-  '#fbb03b',
-  'Black',
-  '#223b53',
-  'Hispanic',
-  '#e55e5e',
-  'Asian',
-  '#3bb2d0',
-  /* other */ '#ccc'
-  ]
-  }
-  },
-  // Place polygons under labels, roads and buildings.
-  'aeroway-polygon'
-  );
-  });
+  
+//apiKey: H1zZ78gYCiKq46kUPJnIyVFzyMzZUUZsjPEXGgl1GZNVoync7C
+//Secret: TdqKye1nvcjk7DuqueR6Av1KteGdSCChuqnZe68t
+
+    
