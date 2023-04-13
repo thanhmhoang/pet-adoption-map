8df2dcace4deb0f4899bcbbf7e9b2f364a6bf310
ChickenChamp71 <chickensandsharks@gmail.com>
2023-04-13 12:03:57 -0700

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,43 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hpY2tlbmNoYW1wNzEiLCJhIjoiY2xnNnlzeXRsMDh2O
     zoom: 9 // starting zoom
 });
 
-
-
+map.on('load', () => {
+  // Add the vector tileset as a source.
+  map.addSource('ethnicity', {
+    type: 'vector',
+    url: 'mapbox://examples.8fgz4egr'
+  });
+  map.addLayer( {
+    'id': 'population',
+    'type': 'circle',
+    'source': 'ethnicity',
+    'source-layer': 'sf2010',
+    'paint': {
+  // Make circles larger as the user zooms from z12 to z22.
+    'circle-radius': {
+    'base': 1.75,
+    'stops': [
+      [12, 2],
+      [22, 180]
+    ]
+  },
+  // Color circles by ethnicity, using a `match` expression.
+  'circle-color': [
+  'match',
+  ['get', 'ethnicity'],
+  'White',
+  '#fbb03b',
+  'Black',
+  '#223b53',
+  'Hispanic',
+  '#e55e5e',
+  'Asian',
+  '#3bb2d0',
+  /* other */ '#ccc'
+  ]
+  }
+  },
+  // Place polygons under labels, roads and buildings.
+  'aeroway-polygon'
+  );
+  });
